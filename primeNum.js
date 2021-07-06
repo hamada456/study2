a5823be5feaa141c976e5c5b53d44b6f5a591b23
hamada456 <73261938+hamada456@users.noreply.github.com>
2021-07-06 12:49:53 +0900

    07061250

--- primeNum.js
+++ primeNum.js
@@ -2,18 +2,27 @@ let primeNumStart = 3;
 let primeNumNext = 0;
 let flag = 0;
 let primeCount = 0;
+let primeArray = [2];
 
-//primeCount
-while(primeNumStart < 10){
+//素数の数が[2]と49個追加まで繰り返す
+while(primeCount < 49){
 primeNumNext = primeNumStart - 1;
+	//割る数が１になったかどうか判定
 	while(primeNumNext != 1){
+		//割り切れたらflag+1
 		if(primeNumStart % primeNumNext === 0){
 			flag++;
 		};
 	primeNumNext--;
 	};
-	if(flag <1){
+	//割り切れなかったらカウントして配列に追加
+	if(flag === 0){
 		primeCount++;
+		primeArray.push(primeNumStart);
 	};
 primeNumStart++;
+flag = 0;
 };
+
+console.log(primeArray);
+document.getElementById("btn").innerText = primeArray
